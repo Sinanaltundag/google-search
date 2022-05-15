df31a70ab0bb616275c7aee02e3cd24c3ad324ab
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-15 22:46:49 +0300

    router reconfigure

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useRoutes } from 'react-router-dom'
 import About from '../components/About'
 import Main from '../components/Main'
+import Results from '../components/Results'
+import SearchBar from '../components/SearchBar'
 
 
 const Router = () => {
-  return (
-    <div>
-
-<Routes>
-        <Route path="/" element={<Main />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
-  )
+  //! aynı componente farklı pathler atama
+  const element = useRoutes([
+  { path: "/", element: <Navigate to="/search" replace={true} /> },
+  { path: "/search", element: <Results /> },
+  { path: "/images", element: <Results /> },
+  { path: "/news", element: <Results /> },
+  { path: "/videos", element: <Results /> },
+  { path: "/about", element: <About /> },
+  ]);
+  return element
 }
 
 export default Router
+
+// (
+//     <div>
+
+// <Routes>
+//         {/* <Route path="/" element={<Main />} /> */}
+//         <Route path="about" element={<About />} />
+//         <Route path="/" element={<Navigate to="/search" replace/>} />
+//         {/* <Route path="/search" element={<Results />} /> */}
+
+//       </Routes>
+//     </div>
+//   )
