1dc5797af94489d15624405529811a9387ee92b4
Sinan Altundağ <87491141+Sinanaltundag@users.noreply.github.com>
2022-05-16 18:51:26 +0300

    router in app.jsx

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import { useState } from 'react';
-import './App.css';
-import Footer from './components/Footer';
-import NavBar from './components/NavBar';
-import Router from './Router/Router';
+import { useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import About from "./components/About";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
+import NavBar from "./components/NavBar";
+import Results from "./components/Results";
 
 function App() {
-
-  const [darkTheme, setDarkTheme] = useState(false)
+  const [darkTheme, setDarkTheme] = useState(false);
   return (
-    <div className={darkTheme? "dark": ""}>
-    <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen">
-<NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme}/>
-<Router/>
-<Footer/>
-
-    </div>
+    <div className={darkTheme ? "dark" : ""}>
+      <div className="bg-gray-100 dark:bg-gray-900 dark:text-gray-200 min-h-screen ">
+        <div className=" flex flex-col justify-between min-h-screen ">
+          <NavBar darkTheme={darkTheme} setDarkTheme={setDarkTheme} />
+          <Routes>
+        {/* <Route index element={<Main />} /> */}
+        <Route path="/" element={<Main />}/>
+          {/* <Route index element={<Results />} /> */}
+          <Route path="search" element={<Results />} />
+          <Route path="image" element={<Results />} />
+          <Route path="video" element={<Results />} />
+          <Route path="news" element={<Results />} />
+        <Route path="*" element={<About />} />
+      </Routes>
+          <Footer />
+        </div>
+      </div>
     </div>
   );
 }
